c71965651d2cb3683c57280023a9600ecbedb317
Andreas Textor <textor.andreas@googlemail.com>
2013-03-30 20:39:08 +0100

    Added start scripts

---
+++ start-linux.sh
@@ -0,0 +1,1 @@
+java -cp bin:lib/hiero.jar:lib/scala-library.jar:lib/ibxm.jar:lib/jinput.jar:lib/jnlp.jar:lib/jogg-0.0.7.jar:lib/jorbis-0.0.15.jar:lib/lwjgl-debug.jar:lib/lwjgl.jar:lib/lzma.jar:lib/packulike.jar:lib/pedigree.jar:lib/scalar.jar:lib/slick.jar:lib/slick-util.jar:lib/tinylinepp.jar -Djava.library.path=lib/linux de.atextor.madv.game.Madv
